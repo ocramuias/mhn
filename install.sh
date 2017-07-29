bb6d6305b2b4407bd085ffea9a7dce17df5cb4f9
Brynjar Fredriksen <bfredriksen@anomali.com>
2017-07-29 13:47:22 -0700

    get Travis working?

--- install.sh
+++ install.sh
@@ -14,47 +14,47 @@ WWW_OWNER="www-data"
 SCRIPTS="$MHN_HOME/scripts/"
 cd $SCRIPTS
 
-if [ -f /etc/redhat-release ]; then
-    export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:$PATH
-    #yum updates + health
-    yum clean all -y
-    yum update -y
-
-    #Dump yum info for troubleshooting
-    echo -e "Yum Repo List:\n"
-    yum repolist
-    echo -e "Yum Dev Group Packages:\n"
-    yum grouplist | grep -i development
-    echo -e "Attempting to install Dev Tools"
-    yum groupinfo mark install "Development Tools"
-    yum groupinfo mark convert "Development Tools"
-    yum groupinstall "Development Tools" -y
-    echo -e "Development Tools successfully installed\n"
-
-    WWW_OWNER="nginx"
-    ./install_sqlite.sh
-
-    if [ ! -f /usr/local/bin/python2.7 ]; then
-        echo "[`date`] Installing Python2.7 as a pre-req"
-       ./install_python2.7.sh
-    fi
-
-     ./install_supervisord.sh
-fi
+#if [ -f /etc/redhat-release ]; then
+#    export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:$PATH
+#    #yum updates + health
+#    yum clean all -y
+#    yum update -y
+#
+#    #Dump yum info for troubleshooting
+#    echo -e "Yum Repo List:\n"
+#    yum repolist
+#    echo -e "Yum Dev Group Packages:\n"
+#    yum grouplist | grep -i development
+#    echo -e "Attempting to install Dev Tools"
+#    yum groupinfo mark install "Development Tools"
+#    yum groupinfo mark convert "Development Tools"
+#    yum groupinstall "Development Tools" -y
+#    echo -e "Development Tools successfully installed\n"
+#
+#    WWW_OWNER="nginx"
+#    ./install_sqlite.sh
+#
+#    if [ ! -f /usr/local/bin/python2.7 ]; then
+#        echo "[`date`] Installing Python2.7 as a pre-req"
+#       ./install_python2.7.sh
+#    fi
+#
+#     ./install_supervisord.sh
+#fi
 
 echo "[`date`] Starting Installation of all MHN packages"
 
 echo "[`date`] ========= Installing hpfeeds ========="
- ./install_hpfeeds.sh
+ sudo  ./install_hpfeeds.sh
 
 echo "[`date`] ========= Installing menmosyne ========="
- ./install_mnemosyne.sh
+ sudo ./install_mnemosyne.sh
 
 echo "[`date`] ========= Installing Honeymap ========="
- ./install_honeymap.sh
+ sudo ./install_honeymap.sh
 
 echo "[`date`] ========= Installing MHN Server ========="
- ./install_mhnserver.sh
+ sudo ./install_mhnserver.sh
 
 echo "[`date`] ========= MHN Server Install Finished ========="
 echo ""
@@ -103,7 +103,7 @@ do
     fi
 done
 
-chown $WWW_OWNER /var/log/mhn/mhn.log
+sudo chown $WWW_OWNER /var/log/mhn/mhn.log
 supervisorctl restart mhn-celery-worker
 
 echo "[`date`] Completed Installation of all MHN packages"
